2a2bd650292b070c765a67e998a18bc9157bb321
AlecMalenfant001 <malenfantalec@gmail.com>
2024-03-21 23:30:23 -0500

    Start working on AdditionClient_v4 and AdditionServer_v4

--- AdditionClient_Hw3_v4.java
+++ AdditionClient_Hw3_v4.java
@@ -8,42 +8,34 @@ import java.io.*;
 import java.util.Scanner;
 
 /**
-   This version of the client will,
-     1) Either send the server a negative integer and go to step 6,
-        or go to step 2.
-     2) Send the server a sequence of positive integers.
-     3) Send the server a negative integer to end the sequence.
-     4) Receive back from the server the sum of the sequence.
-     5) Go to step 1.
-     6) Close the connection to the server.
-*/
-public class AdditionClient_Hw3_v4
-{
+ * This version of the client will,
+ * 1) Either send the server a negative integer and go to step 6,
+ * or go to step 2.
+ * 2) Send the server a sequence of positive integers.
+ * 3) Send the server a negative integer to end the sequence.
+ * 4) Receive back from the server the sum of the sequence.
+ * 5) Go to step 1.
+ * 6) Close the connection to the server.
+ */
+public class AdditionClient_Hw3_v4 {
    public static final int SERVER_PORT = 5000; // Should be above 1023.
 
-   public static void main (String[] args)
-   {
-      Socket          socket = null;
-      BufferedReader  in = null;
-      PrintWriter     out = null;
+   public static void main(String[] args) {
+      Socket socket = null;
+      BufferedReader in = null;
+      PrintWriter out = null;
 
       final String hostName;
-      if (args.length > 0)
-      {
+      if (args.length > 0) {
          hostName = args[0];
-      }
-      else
-      {
+      } else {
          hostName = "localhost";
       }
 
       final int portNumber;
-      if (args.length > 1)
-      {
+      if (args.length > 1) {
          portNumber = Integer.parseInt(args[1]);
-      }
-      else
-      {
+      } else {
          portNumber = SERVER_PORT;
       }
 
@@ -51,31 +43,70 @@ public class AdditionClient_Hw3_v4
       // to identify the client in the server's transcrip.
       final ProcessHandle handle = ProcessHandle.current();
       final long pid = handle.pid();
-      System.out.println("CLIENT: Process ID number (PID): " + pid );
+      System.out.println("CLIENT: Process ID number (PID): " + pid);
 
       // Make a connection to the server
-      try
-      {
-         System.out.println("CLIENT: connecting to server: " + hostName + " on port " + portNumber );
+      try {
+         System.out.println("CLIENT: connecting to server: " + hostName + " on port " + portNumber);
          socket = new Socket(InetAddress.getByName(hostName), portNumber);
 
          in = new BufferedReader(
-                  new InputStreamReader(
-                       socket.getInputStream()));
+               new InputStreamReader(
+                     socket.getInputStream()));
          out = new PrintWriter(socket.getOutputStream());
-      }
-      catch (IOException e)
-      {
+      } catch (IOException e) {
          System.out.println("CLIENT: Cannot connect to server.");
-         //System.out.println( e );
+         // System.out.println( e );
          e.printStackTrace();
          System.exit(-1);
       }
 
       // Implement the appropriate client/server protocol.
 
+      // get the first int to ensure that it is not a sentinal
+      final Scanner stdin = new Scanner(System.in);
+      int outerSentinalCheck = stdin.nextInt();
 
+      while (outerSentinalCheck >= 0) { // an integer value of -1 ends the sequence
+         System.out.println("outerSentinalCheck = " + outerSentinalCheck);
 
+         out.println(outerSentinalCheck); // Send int as a text string on its own line.
+         out.flush(); // send the int
 
+         // read integers and send them to the server
+         int n = 0;
+         while (n >= 0) {
+            n = stdin.nextInt();
+            out.println(n); // Send each int as a text string on its own line.
+            System.out.println("n = " + n);
+         }
+         out.flush(); // send all the ints
+
+         // Receive the server's response.
+         try {
+            final String response = in.readLine();
+            final int sum = Integer.parseInt(response.trim());
+            System.out.println("CLIENT: Server response is: sum = " + sum);
+         } catch (IOException e) {
+            System.out.println("CLIENT: Cannot receive response from server.");
+            System.out.println(e);
+         }
+
+         // get the next number of ints to read
+         outerSentinalCheck = stdin.nextInt();
+
+      }
+      out.println(-1); // Tell the server there are no more ints to read
+      out.flush();
+
+      // Disconnect from the server.
+      try {
+         socket.close();
+         System.out.println("CLIENT: Closed socket");
+      } catch (IOException e) {
+         System.out.println("CLIENT: Cannot disconnect from server.");
+         System.out.println(e);
+      }
    }
+
 }
--- AdditionServer_Hw3_v4.java
+++ AdditionServer_Hw3_v4.java
@@ -7,29 +7,24 @@ import java.net.*;
 import java.io.*;
 
 /**
-   For each client, this server will,
-     1) Read an integer.
-        If the integer is negative,
-        then close the connection to the client.
-     2) Read a sequence of positive integer values,
-        one value per line of text,
-        up until a negative value.
-     3) Send back the sum of the sequence as a text string.
-     4) Go back to step 1.
-*/
-public class AdditionServer_Hw3_v4
-{
+ * For each client, this server will,
+ * 1) Read an integer.
+ * If the integer is negative,
+ * then close the connection to the client.
+ * 2) Read a sequence of positive integer values,
+ * one value per line of text,
+ * up until a negative value.
+ * 3) Send back the sum of the sequence as a text string.
+ * 4) Go back to step 1.
+ */
+public class AdditionServer_Hw3_v4 {
    public static final int SERVER_PORT = 5000; // Should be above 1023.
 
-   public static void main (String[] args)
-   {
+   public static void main(String[] args) {
       final int portNumber;
-      if (args.length > 0)
-      {
+      if (args.length > 0) {
          portNumber = Integer.parseInt(args[0]);
-      }
-      else
-      {
+      } else {
          portNumber = SERVER_PORT;
       }
 
@@ -39,71 +34,99 @@ public class AdditionServer_Hw3_v4
       // to identify the server in TaskManager or TCPView.
       final ProcessHandle handle = ProcessHandle.current();
       final long pid = handle.pid();
-      System.out.println("SERVER: Process ID number (PID): " + pid );
+      System.out.println("SERVER: Process ID number (PID): " + pid);
 
       // Get the name and IP address of the local host and
       // print them on the console for information purposes.
-      try
-      {
+      try {
          final InetAddress address = InetAddress.getLocalHost();
-         System.out.println("SERVER Hostname: " + address.getCanonicalHostName() );
-         System.out.println("SERVER IP address: " +address.getHostAddress() );
+         System.out.println("SERVER Hostname: " + address.getCanonicalHostName());
+         System.out.println("SERVER IP address: " + address.getHostAddress());
          System.out.println("SERVER Using port no. " + portNumber);
-      }
-      catch (UnknownHostException e)
-      {
+      } catch (UnknownHostException e) {
          System.out.println("Unable to determine this host's address.");
-         System.out.println( e );
+         System.out.println(e);
       }
 
       // Create the server's listening socket.
       ServerSocket serverSocket = null;
-      try
-      {
+      try {
          serverSocket = new ServerSocket(portNumber);
          System.out.println("SERVER online:");
-      }
-      catch (IOException e)
-      {
+      } catch (IOException e) {
          System.out.println("SERVER: Error creating network connection.");
-         //System.out.println( e );
+         // System.out.println( e );
          e.printStackTrace();
          System.exit(-1);
       }
 
-      while(true) // Serve multiple clients.
-      {           // Each client can make one request.
+      while (true) // Serve multiple clients.
+      { // Each client can make one request.
          Socket socket = null;
          BufferedReader in = null;
          PrintWriter out = null;
 
          // Wait for an incoming client request.
-         try
-         {
+         try {
             socket = serverSocket.accept();
 
             // At this point, a client connection has been made.
             in = new BufferedReader(
-                     new InputStreamReader(
-                          socket.getInputStream()));
+                  new InputStreamReader(
+                        socket.getInputStream()));
 
             out = new PrintWriter(socket.getOutputStream());
-         }
-         catch(IOException e)
-         {
+         } catch (IOException e) {
             System.out.println("SERVER: Error connecting to client");
-            System.out.println( e );
+            System.out.println(e);
          }
 
          ++clientCounter;
          // Get the IP address of the client and log it to the console.
          final InetAddress clientIP = socket.getInetAddress();
-         System.out.println("SERVER: Client " + clientCounter + ": IP: " +  clientIP.getHostAddress());
+         System.out.println("SERVER: Client " + clientCounter + ": IP: " + clientIP.getHostAddress());
 
          // Implement the appropriate client/server protocol.
 
+         // Read the client's sequence of integer values,
+         // sum them, and send back the sum.
+         try {
+            int sum = 0;
+            String request;
 
+            // read the first line to get the number of ints to read from the client
+            request = in.readLine();
+            int outerSentinalCheck = Integer.parseInt(request);
 
+            int n = 0;
+            while (outerSentinalCheck >= 0) { // an integer value of -1 ends the sequence
+               System.out.println("outerSentinalCheck = " + outerSentinalCheck);
+               sum += outerSentinalCheck;
+
+               while (n >= 0) {
+                  request = in.readLine();
+                  n = Integer.parseInt(request.trim());
+                  sum += n;
+                  System.out.println("n = " + n);
+               }
+               // send sum to client
+               System.out.println("SERVER: Client " + clientCounter + ": Message received: sum = " + sum);
+               out.println(sum); // Send the sum as a text string.
+               out.flush(); // Now make sure that the response is sent.
+               sum = 0;
+
+               // get the next number of ints to read
+               outerSentinalCheck = Integer.parseInt(in.readLine().trim());
+               System.out.println("outerSentinalCheck = " + outerSentinalCheck);
+
+            }
+
+            socket.close(); // We are done with the client's request.
+            System.out.println("SERVER: Client " + clientCounter + ": Closed socket.");
+         } catch (IOException e) {
+            System.out.println("SERVER: Error communicating with client (Client no. " + clientCounter + ")");
+            System.out.println(e);
+         }
 
       }
    }
